7f4e938d23d9722203346f3643983d1c58596cb8
karnelll <165611407+karnelll@users.noreply.github.com>
2024-07-05 14:04:01 +0900

    StatisticsPage 경로 추가 #2

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,13 +1,17 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout";
 import MainPage from "../pages/MainPage/MainPage";
+import StatisticsPage from "../pages/StatisticsPage/StatisticsPage";
 
 const Router = () => (
   <RouterProvider
     router={createBrowserRouter([
       {
         element: <Layout />,
-        children: [{ path: "", element: <MainPage /> }],
+        children: [
+          { path: '/', element: <MainPage /> },
+          { path: '/3', element: <StatisticsPage /> },
+        ],
       },
     ])}
   />

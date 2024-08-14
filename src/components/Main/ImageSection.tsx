314221d25d0b2827890eb5713059721d63c84648
Yoonj310 <opi2982@gmail.com>
2024-08-14 16:50:00 +0900

    mainpage 설명 수정 및 이미지 변경

--- src/components/Main/ImageSection.tsx
+++ src/components/Main/ImageSection.tsx
@@ -19,11 +19,33 @@ const ImageSection: React.FC<ImageSectionProps> = ({
         className="w-1/2 h-auto object-contain rounded-lg mr-6"
       />
       <div className="flex flex-col">
-        <h1 className="text-2xl font-bold mb-2">{descriptions[0]}</h1>
+        <h1 className="text-2xl font-bold mb-2">
+          {descriptions[0].split('\n').map((line, index) => (
+            <React.Fragment key={index}>
+              {line.split('\t').map((subline, subindex) => (
+                <React.Fragment key={subindex}>
+                  {subindex > 0 && <>&nbsp;&nbsp;&nbsp;&nbsp;</>}
+                  {subline}
+                </React.Fragment>
+              ))}
+              <br />
+            </React.Fragment>
+          ))}
+        </h1>
         <ol className="list-decimal list-inside text-lg text-gray-700">
           {descriptions.slice(1).map((desc, index) => (
             <li key={index} className="ml-4">
-              {desc}
+              {desc.split('\n').map((line, idx) => (
+                <React.Fragment key={idx}>
+                  {line.split('\t').map((subline, subindex) => (
+                    <React.Fragment key={subindex}>
+                      {subindex > 0 && <>&nbsp;&nbsp;&nbsp;&nbsp;</>}
+                      {subline}
+                    </React.Fragment>
+                  ))}
+                  <br />
+                </React.Fragment>
+              ))}
             </li>
           ))}
         </ol>
